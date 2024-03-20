763db7307f1d3ae350eafdfb74843818f75ae1b0
zhenw22523 <160691910+zhenw22523@users.noreply.github.com>
2024-03-20 16:55:08 +1300

    awa

---
+++ hw.py
@@ -0,0 +1,15 @@
+#wh1
+minute=int(input("how many minutes?"))
+second=minute*60
+print(second,"seconds")
+
+#hw2
+cards_cruella=int(input("how many cards cruella received?"))
+cards_jane=cards_cruella*2
+print("jane receives",cards_jane,"cards")
+
+#hw3
+year=int(input("how old r u?"))
+day=year*365
+day_last=36500-day
+print("you're",day,"days old. In",day_last,"days you will be 100 years old")
